3176ffe038c818b1336d0a33566bde70eb64fd3c
Istem Karakas <istem.karakas@gmail.com>
2025-03-16 16:09:46 +0000

    Updated requirements

--- env.py
+++ env.py
@@ -1,3 +1,7 @@
 import os
+
+#Set enviroments variables
 os.environ["CLOUDINARY_URL"] = "cloudinary://233818786776195:7w7TGMgDDkCjuERm2v8eIy_90SY@drbyc4bof"
+os.environ.setdefault("SECRET_KEY", "yC7&v9@km*")
 os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = "postgresql://neondb_owner:zSjrJT9cNgF1@ep-falling-darkness-a22ln895.eu-central-1.aws.neon.tech/lemon_shelf_syrup_772590"
