40a37a98dbe39a5be585177d4039db314023cf90
Istem Karakas <istem.karakas@gmail.com>
2025-05-07 14:41:14 +0100

    Debug API request for most followed profiles

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,7 +58,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = 'DEV' in os.environ
+
 
 ALLOWED_HOSTS = [
     'localhost',
